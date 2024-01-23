fdaaac835e2a9a57ab3648d98501a298569bc2f7
Sandy <sandychen123456789@gmail.com>
2024-01-23 21:07:19 +0800

    新增BMI js端輸出數字&文字評語

--- js/bmi.js
+++ js/bmi.js
@@ -22,4 +22,25 @@ function ClickBMI() {
         alert("不可為空值")
     }
     console.log(bmi);
+    BMIEL.innerHTML = "BMI=" + bmi.toFixed(2)
+
+    //
+    if (bmi < 18.5) {
+        resultBMI = "過輕"
+    } else if (bmi < 24) {
+        resultBMI = "正常範圍"
+    } else if (bmi < 27) {
+        resultBMI = "過重"
+    } else if (bmi < 30) {
+        resultBMI = "輕度肥胖"
+    } else if (bmi < 35) {
+        resultBMI = "中度肥胖"
+    } else {
+        resultBMI = "重度肥胖"
+    }
+    ResultEL.innerHTML = resultBMI
 }
+const BMIEL = document.querySelector("#bmi");
+console.log(BMIEL);
+const ResultEL = document.querySelector("#comment");
+console.log(ResultEL);
